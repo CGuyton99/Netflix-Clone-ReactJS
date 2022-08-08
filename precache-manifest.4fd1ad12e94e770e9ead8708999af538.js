5712a5c3e154c7c74ea4183bf0d1ec008b3d42f5
Christian Guyton <cguyton@icstars.org>
2022-08-08 16:28:30 -0500

    Updates

---
+++ precache-manifest.4fd1ad12e94e770e9ead8708999af538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a49f0b8f59e298e1c1e0646258016c",
+    "url": "/Netflix-Clone-ReactJS/index.html"
+  },
+  {
+    "revision": "ef47ca456d46c2221c60",
+    "url": "/Netflix-Clone-ReactJS/static/css/main.51abe0fe.chunk.css"
+  },
+  {
+    "revision": "5dc099c1e57f337774de",
+    "url": "/Netflix-Clone-ReactJS/static/js/2.f04bfa69.chunk.js"
+  },
+  {
+    "revision": "7e9a586c4d29df81a0106ce0dcaf059d",
+    "url": "/Netflix-Clone-ReactJS/static/js/2.f04bfa69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef47ca456d46c2221c60",
+    "url": "/Netflix-Clone-ReactJS/static/js/main.ba8b13c5.chunk.js"
+  },
+  {
+    "revision": "0066ce3c725e37e425ef",
+    "url": "/Netflix-Clone-ReactJS/static/js/runtime-main.17c8f407.js"
+  }
+]);
